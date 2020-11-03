acca14e84dd060a58b093ffa868d2bdb61aa7e3c
Luke <lflint07@gmail.com>
2020-11-02 20:58:29 -0500

    added components and styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css'
 
 function BookList() {
   return (
     <section>
-      this is a booklist
+      <Book />
     </section>
   );
 }
@@ -13,16 +14,18 @@ const Book = () => {
   return (
     <article>
       <Image></Image>
+      <Title />
+      <Author />
     </article>
   );
 };
 
-const Image = () => {
-  <img>
-    
-  </img>
-}
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg" />
+);
+
+const Title = () => <h1>Greenlights</h1>;
+
+const Author = () => <h2>Matthew McConaughey</h2>
 
 ReactDom.render(<BookList />, document.getElementById("root"));
-
-
